91d4e0ba985efd142392398730ddd0f64960b7f6
Jérôme du Camp d Orgas <ducampdorgasjerome@gmail.com>
2023-09-02 11:23:18 +0200

    connexion NP: id=false,mdp=true + id=true,mdp=empty

--- tests/connexion.test.js
+++ tests/connexion.test.js
@@ -17,12 +17,24 @@ test.describe.only("ruv_test1", () => {
     await page.locator(".navbar-right .btn-default").click();
     await expect(page).toHaveURL(/.*signIn=1/);
   });
-  test.only("Connexion NP:id true, mdp false", async ({ page }) => {
+  test("Connexion NP:id true, mdp false", async ({ page }) => {
     await page.locator("#username").fill(user);
     await page.locator("#password").fill(MDPP + "s");
-
     await page.locator("#submit").click();
     await expect(page.locator(".alert.alert-danger")).toBeVisible();
+  });
+  test("Connexion NP:id false, mdp true", async ({ page }) => {
+    await page.locator("#username").fill(user + "s");
+    await page.locator("#password").fill(MDPP);
+    await page.locator("#submit").click();
+    await expect(page.locator(".alert.alert-danger")).toBeVisible();
+    //await page.pause();
+  });
+  test.only("Connexion NP:id true, mdp empty", async ({ page }) => {
+    await page.locator("#username").fill(user);
+    await page.locator("#password").fill("");
+    await page.locator("#submit").click();
+    await expect(page).toHaveURL(/.*signIn=1/);
     //await page.pause();
   });
 });
