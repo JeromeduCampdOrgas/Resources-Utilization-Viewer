933c2de11596ae877271744700240e11be42c4f6
Jérôme du Camp d Orgas <ducampdorgasjerome@gmail.com>
2023-09-02 14:16:46 +0200

    Connexion NP:id true, mdp empty" + ...

--- tests/connexion.test.js
+++ tests/connexion.test.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { user, MDPP } from "../variables/variablesCX";
 
-test.describe.only("ruv_test1", () => {
+test.describe.only("test_connexion", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(
       "http://localhost/resources-utilization-viewer/app/index.php"
@@ -30,8 +30,29 @@ test.describe.only("ruv_test1", () => {
     await expect(page.locator(".alert.alert-danger")).toBeVisible();
     //await page.pause();
   });
-  test.only("Connexion NP:id true, mdp empty", async ({ page }) => {
+  test("Connexion NP:id true, mdp empty", async ({ page }) => {
     await page.locator("#username").fill(user);
+    await page.locator("#password").fill("");
+    await page.locator("#submit").click();
+    await expect(page).toHaveURL(/.*signIn=1/);
+    //await page.pause();
+  });
+  test("Connexion NP:id empty, mdp true", async ({ page }) => {
+    await page.locator("#username").fill("");
+    await page.locator("#password").fill(MDPP);
+    await page.locator("#submit").click();
+    await expect(page).toHaveURL(/.*signIn=1/);
+    //await page.pause();
+  });
+  test("Connexion NP:id empty, mdp false", async ({ page }) => {
+    await page.locator("#username").fill("");
+    await page.locator("#password").fill(MDPP + "s");
+    await page.locator("#submit").click();
+    await expect(page).toHaveURL(/.*signIn=1/);
+    //await page.pause();
+  });
+  test("Connexion NP:id empty, mdp empty", async ({ page }) => {
+    await page.locator("#username").fill("");
     await page.locator("#password").fill("");
     await page.locator("#submit").click();
     await expect(page).toHaveURL(/.*signIn=1/);
